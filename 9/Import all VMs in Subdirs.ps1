9974e806727acd5e3e82461bebdeb661e0f96544
Ariel Silverstone <arielsilverstone@users.noreply.github.com>
2025-09-16 13:17:57 -0700

    Delete 9 directory

--- 9/Import all VMs in Subdirs.ps1
+++
@@ -1,2 +0,0 @@
-Get-ChildItem d:\vm -Recurse -Filter "Virtual Machines" | %{Get-ChildItem lab\ -Recurse -Filter "Virtual Machines" | %{Get-ChildItem $_.FullName -Filter *.vmcx} | %{import-vm $_.FullName -Register}} | %{import-vm $_.FullName -Register}
-
