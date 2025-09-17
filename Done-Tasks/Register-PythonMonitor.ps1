b974c5f23a653933077bbfe7fbfb43f2dc83af16
Ariel Silverstone <arielsilverstone@users.noreply.github.com>
2025-09-16 17:13:32 -0700

    Delete Done-Tasks directory

--- Done-Tasks/Register-PythonMonitor.ps1
+++
@@ -1,5 +0,0 @@
-# Register-PythonMonitor.ps1
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File D:\Program Files\Dev\Scripts\Monitor-PythonDirs.ps1"
-$trigger = New-ScheduledTaskTrigger -Daily -At 9am
-$settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -WakeToRun
-Register-ScheduledTask -TaskName "Monitor Python Installations" -Action $action -Trigger $trigger -Settings $settings -RunLevel Highest
